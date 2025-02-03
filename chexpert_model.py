ab85a54362d521aa10426d95dfdbe671ae337fc2
Sutariya <dishantkumar.sutariya@mevis.fraunhofer.de>
2025-02-03 14:24:49 +0100

    Add Evalutiob code

--- chexpert_model.py
+++ chexpert_model.py
@@ -52,11 +52,11 @@ class MyDataset(Dataset):
 
         return img, label
 
-def store_images_labels(training_data_merge):
+def store_diagnostic_images_labels(training_data_merge):
      data_images = []
      paths = tqdm(training_data_merge['Path'], desc="Loading images")
      for path in paths:
-          full_path = '../datasets' + '/' + str(path)
+          full_path = '../../datasets' + '/' + str(path)
           img = read_image(full_path)
           data_images.append(img)
           paths.set_postfix({'Loaded': len(data_images)})
@@ -65,6 +65,22 @@ def store_images_labels(training_data_merge):
      data_labels = torch.tensor(data_labels, dtype=torch.float32)
 
      return data_images, data_labels
+
+def store_race_images_labels(training_data_merge):
+    data_images = []
+    training_data_merge = training_data_merge[training_data_merge['race'].isin(training_data_merge['race'].value_counts()[:5].index)]
+    paths = tqdm(training_data_merge['Path'], desc="Loading images")
+    for path in paths:
+         full_path = '../../datasets' + '/' + str(path)
+         img = read_image(full_path)
+         data_images.append(img)
+         paths.set_postfix({'Loaded': len(data_images)})
+    training_data_merge = pd.get_dummies(training_data_merge, columns=['race'], dtype=float)
+    data_labels = training_data_merge[['race_Asian', 'race_Black', 'race_Other', 'race_Unknown', 'race_White']].values
+    data_labels = torch.tensor(data_labels, dtype=torch.float32)
+    data_labels = data_labels.argmax(dim=1) 
+
+    return data_images, data_labels
 
 def prepare_dataloaders(data_images, labels):
      transform = transforms.Compose([
@@ -91,11 +107,8 @@ def model_building(in_feature, out_feature, weights=None):
 
      return model
 
-def model_training(model, train_loader, val_loader, criterion, optimizer, num_epochs=10, device=None):
+def model_training(model, train_loader, val_loader, criterion, optimizer, num_epochs=10, device=None, is_binary=False):
     
-    if device is None:
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
     model = model.to(device)
     
     for epoch in range(num_epochs):
@@ -111,14 +124,21 @@ def model_training(model, train_loader, val_loader, criterion, optimizer, num_ep
             optimizer.zero_grad()
             
             outputs = model(inputs).to(device)
-            loss = criterion(outputs, labels.float().view(-1, 1).to(device))
-        
+            if is_binary:
+                labels = labels.float().view(-1, 1)
+            else:
+                labels = labels
+            loss = criterion(outputs, labels)
+
             loss.backward()
             optimizer.step()
             
             running_loss += loss.item()
-            predicted = (torch.sigmoid(outputs) > 0.5).float().to(device)
-            correct += (predicted == labels.view(-1, 1)).sum().item()
+            if is_binary:
+                predicted = (torch.sigmoid(outputs) > 0.5).float().to(device)
+            else:
+                _, predicted = torch.max(outputs, 1)
+            correct += (predicted == labels).sum().item()
             total += labels.size(0)
             
             train_loop.set_postfix(loss=loss.item(), acc=correct / total)
@@ -137,11 +157,18 @@ def model_training(model, train_loader, val_loader, criterion, optimizer, num_ep
                 inputs, labels = inputs.to(device), labels.to(device)
                 
                 outputs = model(inputs).to(device)
-                loss = criterion(outputs, labels.float().view(-1, 1).to(device))
+                if is_binary:
+                    labels = labels.float().view(-1, 1)
+                else:
+                    labels = labels
+                loss = criterion(outputs, labels)
 
                 val_loss += loss.item()
-                predicted = (torch.sigmoid(outputs) > 0.5).float().to(device)
-                val_correct += (predicted == labels.view(-1, 1)).sum().item()
+                if is_binary:
+                    predicted = (torch.sigmoid(outputs) > 0.5).float().to(device)
+                else:
+                    _, predicted = torch.max(outputs, 1)
+                val_correct += (predicted == labels).sum().item()
                 val_total += labels.size(0)
                 
                 val_loop.set_postfix(loss=loss.item(), acc=val_correct / val_total)
@@ -153,17 +180,42 @@ def model_training(model, train_loader, val_loader, criterion, optimizer, num_ep
               f"Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f}, "
               f"Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.4f}")
 
+def testing_model(test_loader, model, device):
+
+    model.load_state_dict(torch.load('no_finding_model.pth', map_location=device))
+    model.to(device)
+    model.eval()
+
+    correct = 0
+    total = 0
+
+    loader = tqdm(test_loader)
+    with torch.no_grad():
+        for inputs, labels in loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            predicted = (torch.sigmoid(outputs) > 0.5).float()
+            
+            correct += (predicted == labels.view(-1, 1)).sum().item()
+            total += labels.size(0)
+            loader.set_postfix(acc=correct / total if total > 0 else 0)
+
+    test_acc = correct / total if total > 0 else 0
+    print(f"Test Accuracy: {test_acc:.4f}")
+
 
 
 if __name__ == '__main__':
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     print("hello")
-    training_file_path =  r'..\datasets\CheXpert-v1.0-small\train.csv'
-    demographic_data_path = r'..\datasets\CheXpert-v1.0-small\demographics_CXP.csv'
+    training_file_path =  r'..\..\datasets\CheXpert-v1.0-small\train.csv'
+    demographic_data_path = r'..\..\datasets\CheXpert-v1.0-small\demographics_CXP.csv'
     training_data, demographic_data = file_load(training_file_path, demographic_data_path)
     training_data_merge = merge_dataframe(training_data, demographic_data)
 
-    data_images,labels = store_images_labels(training_data_merge)
+    data_images,labels = store_diagnostic_images_labels(training_data_merge)
     train_loader, val_loader, test_loader = prepare_dataloaders(data_images,labels)
     model = model_building(in_feature=1, out_feature=1)
 
@@ -171,5 +223,9 @@ if __name__ == '__main__':
     loss = nn.BCEWithLogitsLoss()
     optimiser = torch.optim.Adam(model.parameters(),lr)
 
-    model_training(model,train_loader,val_loader,loss,optimiser,10)
-    print("end")
+    model_training(model,train_loader,val_loader,loss,optimiser,10,is_binary=True)
+
+    torch.save(model.state_dict(), 'no_finding_model.pth')
+
+    #testing_model(test_loader,model,device=device)
+    
